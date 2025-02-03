b761c9fe481aff35b56e60e3132ab3418a1373bf
salcines <salcines@outlook.es>
2025-02-03 02:10:35 +0100

    Init commit

---
+++ ClasicalEncryptor/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ClasicalEncryptor;
+
+public partial class MainPage : ContentPage
+{
+    
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+  
+}
---
+++ ClasicalEncryptor/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ClasicalEncryptor;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
